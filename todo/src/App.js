8f91a910d093d83fd2426acf16d465ae9f39cf3f
y-arimura1222 <y.arimura.carecon@gmail.com>
2021-08-21 15:23:16 +0900

    雛形作成

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,7 +1,33 @@
 export const App = () => {
   return (
-    <div className="App">
-      React
-    </div>
+    <>
+      <div className="input-area">
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
+        <ul>
+          <li>
+            <div className="list-row">
+              <p>例) 食事</p>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div className="complete-area">
+        <p className="title">完了したTODO</p>
+        <ul>
+          <li>
+            <div className="list-row">
+              <p>例) 運動</p>
+              <button>戻す</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 }
